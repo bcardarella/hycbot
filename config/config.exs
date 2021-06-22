d1c417af2fbcfa876dc22f6855b8554a06cdd8ef
Brian Cardarella <bcardarella@gmail.com>
2021-06-21 20:42:06 -0400

    Updated schedule

--- config/config.exs
+++ config/config.exs
@@ -4,9 +4,8 @@ config :elixir, :time_zone_database, Tzdata.TimeZoneDatabase
 
 config :hycbot, HYCBot.Scheduler,
   jobs: [
-    {"00 16 * * FRI", {HYCBot, :render_print, [:laser_friday]}}
-    {"00 11 * * SUN", {HYCBot, :render_print, [:laser_sunday]}}
-
+    {"00 16 * * FRI", {HYCBot, :post_conditions, [:laser_friday]}},
+    {"00 11 * * SUN", {HYCBot, :post_conditions, [:laser_sunday]}}
   ],
   debug_logging: false,
   timezone: "America/New_York"
